1f0b591d7076e16eb3a31f6bd80fbdf581c67a49
portermahoney <portermahoney@gmail.com>
2017-11-27 15:14:19 -0500

    testing out pca

--- lipidx/views.py
+++ lipidx/views.py
@@ -8,6 +8,8 @@ import sys, os
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import HoverTool, Whisker, ColumnDataSource, Span, Range1d
 from bokeh.embed import components
+from sklearn.decomposition import PCA
+import numpy
 
 @app.route('/lipid_analysis/', methods=['GET', 'POST'])
 def lipid_analysis():
@@ -50,6 +52,46 @@ def lipid_analysis():
 
 @app.route('/volcano/', methods=['GET', 'POST'])
 def volcano():
+    form_data = request.form
+    form = forms.VolcanoForm()
+    zip_path = None
+    script = None
+    div = None
+    context = {}
+    if form.validate_on_submit():
+        root_path = app.config['UPLOAD_FOLDER']
+        file1 = request.files[form.file1.name]
+        file1.save(root_path + 'file1.txt')
+        file1_path = root_path + 'file1.txt'
+
+        la = LipidAnalysis([file1_path])
+        context['volcano_script'], context['volcano_div'] = la.volcano_plot(form.data)
+        zip_path = la.write_results()
+    return render_template('volcano.html', form=form, zip_path=zip_path, **context)
+
+@app.route('/pca/', methods=['GET', 'POST'])
+def pca():
+    path = '/Users/portermahoney/sites/lipidx_dev/lipidx/lipidx/files/compounds_mc.csv'
+    samples = []
+    names = []
+    data = []
+    with open(path,'r') as f:
+        for ln in f:
+            row = ln.split(',')
+            if 'name' in ln:
+                samples = row
+            else:
+                names.append(row[0])
+                data.append([float(x.replace('/n', '')) for x in row[1:]])
+    cnt = len(data[0])
+    #rand = numpy.random.RandomState(1)
+    #data = numpy.dot(rand.rand(2,2), rand.randn(2,10)).T
+    pca = PCA(n_components=cnt)
+    pca.fit(data)
+    print(pca)
+    print(pca.components_)
+    print(pca.explained_variance_)
+    print('hello')
     form_data = request.form
     form = forms.VolcanoForm()
     zip_path = None
