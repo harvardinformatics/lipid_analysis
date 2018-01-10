d3305eb095dbc617b6198259f89257fa7bb9b383
portermahoney <portermahoney@gmail.com>
2018-01-10 15:55:29 -0500

    fixing config

--- lipidx/config.py
+++ lipidx/config.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('LIPIDX_KEY','n`}UyR_r+9w2]%xZ~H?FRz^')
 loglevel = os.environ.get('LIPIDX_LOGLEVEL','ERROR')
 if loglevel == 'DEBUG':
     DEBUG = True
-WTF_CSRF_SECRET_KEY = (os.environ.get('WTF_CSRF_KEY',"aEsu'a}-j\>rJ4'8MFz{<yn")
+WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_KEY',"aEsu'a}-j\>rJ4'8MFz{<yn")
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'files/')
 FILE_FOLDER = os.path.join(BASE_DIR, 'files/')
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpeg', 'gif', 'doc', 'xls', 'csv'])
