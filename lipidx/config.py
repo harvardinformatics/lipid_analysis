edbd1375fed925e4cbc2e8eb1cb4b6bc5aec039e
portermahoney <portermahoney@gmail.com>
2019-12-16 14:40:36 -0500

    increase flask post limit

--- lipidx/config.py
+++ lipidx/config.py
@@ -12,5 +12,5 @@ WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_KEY',"aEsu'a}-j\>rJ4'8MFz{<yn")
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'files/')
 FILE_FOLDER = os.path.join(BASE_DIR, 'files/')
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpeg', 'gif', 'doc', 'xls', 'csv'])
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+MAX_CONTENT_LENGTH = 100 * 1024 * 1024
 SEND_FILE_MAX_AGE_DEFAULT = 0
